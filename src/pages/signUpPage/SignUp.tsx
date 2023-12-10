9cdfcffc7b936d2e3604b452a3fc023adaeae935
Dor Elazar <dorelazar07@gmail.com>
2023-12-10 14:18:33 +0200

    moved buttons on card, changed LoginBar and removed some useless

--- src/pages/signUpPage/SignUp.tsx
+++ src/pages/signUpPage/SignUp.tsx
@@ -1,48 +1,49 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import UpperBar from '../LoginPage/components/UpperBar';
-import { RoutesEnum } from '../../types/routes.enum';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import UpperBar from "../LoginPage/components/LoginBar";
+import { RoutesEnum } from "../../types/routes.enum";
 
 function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
-
-
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
 const defaultTheme = createTheme();
 
-
 let incorrectMail = false;
 let weakPassword = false;
 let usedMail = false;
 let errorMessage = "";
-
 
 function MailIncorrectness(email: any) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -54,29 +55,38 @@ function PasswordIncorrectness(password: any) {
 }
 
 export default function SignUp() {
-
   function CheckValidation(userData: any) {
-    (MailIncorrectness(userData['username']))? incorrectMail = true :
-    (PasswordIncorrectness(userData['password']))? weakPassword = true :
-    axios.post("http://localhost:4000/register" ,userData).then((response) => {
-      navigate('/')}).catch((error) =>
-      {usedMail = true;
-        errorMessage = error.response.data.error;
-      navigate(RoutesEnum.REGISTER);})
+    MailIncorrectness(userData["username"])
+      ? (incorrectMail = true)
+      : PasswordIncorrectness(userData["password"])
+      ? (weakPassword = true)
+      : axios
+          .post("http://localhost:4000/register", userData)
+          .then((response) => {
+            navigate("/");
+          })
+          .catch((error) => {
+            usedMail = true;
+            errorMessage = error.response.data.error;
+            navigate(RoutesEnum.REGISTER);
+          });
   }
 
-  const handleSignInClick= ()=>{
-    navigate('/');
-  }
+  const handleSignInClick = () => {
+    navigate("/");
+  };
   const handleSubmit = (event: any) => {
-
     incorrectMail = false;
     weakPassword = false;
     usedMail = false;
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const dataToSend = {username: data.get('email'), password: data.get('password'),
-    fName: data.get('firstName'), lName: data.get('lastName')};
+    const dataToSend = {
+      username: data.get("email"),
+      password: data.get("password"),
+      fName: data.get("firstName"),
+      lName: data.get("lastName"),
+    };
     CheckValidation(dataToSend);
 
     navigate(RoutesEnum.REGISTER);
@@ -89,25 +99,30 @@ export default function SignUp() {
       <UpperBar />
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        
+
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: "#6352B1" }}>
             <LockOutlinedIcon />
-          </Avatar >
+          </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} >
-                <TextField 
+              <Grid item xs={12} sm={6}>
+                <TextField
                   autoComplete="given-name"
                   name="firstName"
                   required
@@ -115,8 +130,7 @@ export default function SignUp() {
                   id="firstName"
                   label="First Name"
                   autoFocus
-                  sx={{ '& fieldset': { borderColor: '#6352B1' } }}
-            
+                  sx={{ "& fieldset": { borderColor: "#6352B1" } }}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -127,7 +141,7 @@ export default function SignUp() {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
-                  sx={{ '& fieldset': { borderColor: '#6352B1' } }}
+                  sx={{ "& fieldset": { borderColor: "#6352B1" } }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -138,7 +152,7 @@ export default function SignUp() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
-                  sx={{ '& fieldset': { borderColor: '#6352B1' } }}
+                  sx={{ "& fieldset": { borderColor: "#6352B1" } }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -150,22 +164,36 @@ export default function SignUp() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
-                  sx={{ '& fieldset': { borderColor: '#6352B1' } }}
+                  sx={{ "& fieldset": { borderColor: "#6352B1" } }}
                 />
               </Grid>
-             
             </Grid>
 
             <div>
-            {
-              (!incorrectMail && !weakPassword && !usedMail)? null :
-              <Box style={{border: '2px solid red', borderRadius: '10px', marginTop:'20px',
-              width: 'fullWidth', height: '50px', padding: '10px', color: 'red',
-              display: 'flex',justifyContent: 'center'}}> 
-              <ErrorOutlineIcon />  {incorrectMail? <p>invalid email address</p>:
-              weakPassword? <p>your password is too weak</p>:
-              <p>{errorMessage}</p>}</Box> 
-            }
+              {!incorrectMail && !weakPassword && !usedMail ? null : (
+                <Box
+                  style={{
+                    border: "2px solid red",
+                    borderRadius: "10px",
+                    marginTop: "20px",
+                    width: "fullWidth",
+                    height: "50px",
+                    padding: "10px",
+                    color: "red",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <ErrorOutlineIcon />{" "}
+                  {incorrectMail ? (
+                    <p>invalid email address</p>
+                  ) : weakPassword ? (
+                    <p>your password is too weak</p>
+                  ) : (
+                    <p>{errorMessage}</p>
+                  )}
+                </Box>
+              )}
             </div>
 
             <Button
@@ -178,15 +206,18 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-              <button onClick={handleSignInClick} style={{
-                  color: '#6352B1',
-                  background: 'none',
-                  border: 'none',
-                  textDecoration: 'underline',
-                  cursor: 'pointer',
-                }}>
-                 Already have an account? Sign in
-                  </button>
+                <button
+                  onClick={handleSignInClick}
+                  style={{
+                    color: "#6352B1",
+                    background: "none",
+                    border: "none",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  }}
+                >
+                  Already have an account? Sign in
+                </button>
               </Grid>
             </Grid>
           </Box>
