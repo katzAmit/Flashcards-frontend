241df2d007eb6eb53eebc4c1a89470b99e379f19
Dor Elazar <dorelazar07@gmail.com>
2023-12-10 20:21:32 +0200

    added delete option

--- src/pages/Homepage/components/CardsLayout.tsx
+++ src/pages/Homepage/components/CardsLayout.tsx
@@ -1,31 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Container, Button } from "@mui/material";
 import { Card } from "../../../components/Card";
 import { CardGhost } from "../../../components/CardGhost";
 import { FlashCard } from "../../../types/card.interface";
 import { TiDocumentAdd } from "react-icons/ti";
 import { BiFilterAlt } from "react-icons/bi";
+import axios from "axios";
 
 interface CardsLayoutProps {
   cards?: FlashCard[];
+  deleteFlashCard: (id: number) => void;
 }
-const CardsLayout: React.FC<CardsLayoutProps> = (props) => {
-  const [cards, setCards] = useState<FlashCard[] | undefined>(props.cards);
-
-  function addCard(newCard: any) {
-    setCards((prevCards) => {
-      return [...(prevCards ?? []), newCard];
-    });
-  }
-
-  function deleteCard(id: any) {
-    setCards((prevCards) => {
-      return prevCards?.filter((card, index) => {
-        return index !== id;
-      });
-    });
-  }
-
+const CardsLayout: React.FC<CardsLayoutProps> = ({
+  cards,
+  deleteFlashCard,
+}) => {
   return (
     <Container sx={{ py: 8 }} maxWidth="lg">
       <Grid>
@@ -38,23 +27,23 @@ const CardsLayout: React.FC<CardsLayoutProps> = (props) => {
           </button>
         </div>
         <Grid container spacing={4}>
-          {props.cards
-            ? props.cards?.map((card, index) => (
+          {cards
+            ? cards.map((card) => (
                 <Grid item key={card.id} xs={12} sm={6} md={4}>
                   <Card
                     id={card.id}
                     question={card.question}
                     answer={card.answer}
                     category={card.category}
+                    onDelete={() => deleteFlashCard(card.id)}
                   />
                 </Grid>
               ))
             : new Array(9).fill(0).map((_, index) => (
                 <Grid item key={index} xs={12} sm={6} md={4}>
-                  <CardGhost></CardGhost>
+                  <CardGhost key={index} />
                 </Grid>
-              )
-              )}
+              ))}
         </Grid>
       </Grid>
     </Container>
