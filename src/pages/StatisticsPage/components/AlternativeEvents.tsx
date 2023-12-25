eb55c79a83133ccf0126eeed07f0e8865a0117d0
katzAmit <katz.amit.mail@gmail.com>
2023-12-25 02:52:51 +0200

    stats done

--- src/pages/StatisticsPage/components/AlternativeEvents.tsx
+++ src/pages/StatisticsPage/components/AlternativeEvents.tsx
@@ -1,28 +1,62 @@
 import { colors } from '@mui/material';
-import React from 'react';
-import { VictoryBar, VictoryChart,Bar, VictoryLine, VictoryAxis } from 'victory';
+import { VictoryBar, VictoryChart, VictoryLine, VictoryAxis } from 'victory';
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 export default function AlternativeEvents(props: any) {
+ 
+   //const demoUrl = 'https://codesandbox.io/s/simple-bar-chart-tpz8r';
     
     let clicked: false;
-        
-    return(
-        <VictoryChart height={300} width={300}
-          domainPadding={{ x: 50, y: [0, 20] }}
-          scale={{ x: "time" }}
-        >
-          <VictoryBar 
-            dataComponent={
-              <Bar events={{ }}/>
-            
-            }
-            style={{ data: { fill: "#83f28f" } }}
-            data={props.data}
-          />
-        </VictoryChart>
 
-    );
-}
+    
+  
+    return (
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart
+          width={500}
+          height={300}
+          data={props.data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          {/* <CartesianGrid strokeDasharray="3 3" /> */}
+          <XAxis dataKey="category" tick={false}/>
+          <YAxis />
+          <Tooltip />
+          {/* <Legend /> */}
+          {/* const colorScale = ["#83f28f", "#F7E967", "#F1433F"]; */}
+          <Bar dataKey="questions"  fill="#83f28f" activeBar={<Rectangle fill="#6352B1" stroke="blue" />} />
+          <Bar dataKey="easy"  fill="#83f28f" activeBar={<Rectangle fill="#6352B1" stroke="blue" />} />
+          <Bar dataKey="medium"  fill="#F7E967" activeBar={<Rectangle fill="#6352B1" stroke="blue" />} />
+          <Bar dataKey="hard"  fill="#F1433F" activeBar={<Rectangle fill="#6352B1" stroke="blue" />} />
+          {/* <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} /> */}
+        </BarChart>
+      </ResponsiveContainer>
+    );}
+  
+        
+    // return(
+    //     <VictoryChart height={300} width={300}
+    //       domainPadding={{ x: 50, y: [0, 20] }}
+    //       scale={{ x: "time" }}
+    //     >
+    //       <VictoryBar 
+    //         dataComponent={
+    //           <Bar events={{ }}/>
+            
+    //         }
+    //         style={{ data: { fill: "#83f28f" } }}
+    //         data={props.data}
+    //       />
+    //     </VictoryChart>
+
+    // );
+
 
 // class AlternativeEvents extends React.Component {
 //     constructor() {
